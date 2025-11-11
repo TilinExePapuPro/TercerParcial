199be5533ce857259a5a4ba0d694936451c7cdb3
ArielGrx9 <ariel.devjs@gmail.com>
2025-11-10 23:15:52 -0600

    ahora si lol

--- ArbolBin/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.tercerparcial.arbolbin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.tercerparcial.arbolbin to javafx.fxml;
-    exports org.tercerparcial.arbolbin;
-}
